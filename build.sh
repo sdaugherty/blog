255f793b6ac3bb3cb19cf70af2d5835e68726b17
Stephanie Daugherty <sdaugherty@gmail.com>
2021-02-27 10:45:19 -0600

    Switch to hugo extended

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-DOWNLOAD=https://github.com/gohugoio/hugo/releases/download/v$HUGOVERSION/hugo_${HUGOVERSION}_Linux-64bit.tar.gz
+DOWNLOAD=https://github.com/gohugoio/hugo/releases/download/v$HUGOVERSION/hugo_extended_${HUGOVERSION}_Linux-64bit.tar.gz
 wget --quiet -c $DOWNLOAD -O - | tar -xz
 
 ./hugo
