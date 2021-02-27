26cecba9458f5f8bcfed621d062ade5d0d49250c
Stephanie Daugherty <sdaugherty@gmail.com>
2021-02-26 22:42:48 -0600

    Update routes and hugo version

--- build.sh
+++ build.sh
@@ -3,6 +3,4 @@
 DOWNLOAD=https://github.com/gohugoio/hugo/releases/download/v$HUGOVERSION/hugo_${HUGOVERSION}_Linux-64bit.tar.gz
 wget --quiet -c $DOWNLOAD -O - | tar -xz
 
-git submodule init --recursive
-git submodule update --recursive
 ./hugo
